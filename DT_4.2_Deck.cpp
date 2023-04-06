b48740dbc77779120472bbd6796d3a45e3dbdf0f
m1ngu01 <89462792+m1ngu01@users.noreply.github.com>
2023-04-06 12:49:19 +0900

    Add files via upload

---
+++ DT_4.2_Deck.cpp
@@ -0,0 +1,90 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_QUEUE_SIZE 100
+
+typedef int Element;
+Element data[MAX_QUEUE_SIZE];
+int front;
+int rear;
+
+//forward declaration
+void enqueue(Element val);
+Element dequeue();
+Element peek();
+
+void init_queue() { front = rear = 0; ; }
+int is_empty() { return front == rear;; }
+int is_full() { return (rear + 1) % MAX_QUEUE_SIZE == front; }
+int size() { return (rear - front + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE; }
+void init_deque() { init_queue(); }
+void add_rear(Element val) { enqueue(val); }
+Element delete_front() { return dequeue(); }
+Element get_front() { return peek(); }
+
+void error(const char str[]) {
+	printf("%s\n", str);
+	exit(1);
+}
+void print_queue(const char msg[]) {
+	int i, maxi = rear;
+	if (front >= rear) maxi += MAX_QUEUE_SIZE;
+	printf("%s[% 2d] = ", msg, size());
+	for (i = front + 1; i <= maxi; i++)
+		printf("%2d ", data[i % MAX_QUEUE_SIZE]);
+	printf("\n");
+}
+void enqueue(Element val) {
+	if (is_full())
+		error(" 큐 포화 에러");
+	rear = (rear + 1) % MAX_QUEUE_SIZE;
+	data[rear] = val;
+}
+Element dequeue() {
+	if (is_empty())
+		error(" 큐 공백 에러");
+	front = (front + 1) % MAX_QUEUE_SIZE;
+	return data[front];
+}
+Element peek() {
+	if (is_empty())
+		error(" 큐 공백 에러");
+	return data[(front + 1) % MAX_QUEUE_SIZE];
+}
+
+void add_front(Element val) {
+	if (is_full())
+		error(" 덱 포화 에러");
+	data[front] = val;
+	front = (front - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+}
+Element delete_rear() {
+	Element ret;
+	if (is_empty())
+		error(" 덱 공백 에러");
+	ret = data[rear];
+	rear = (rear - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+	return ret;
+}
+Element get_rear() {
+	if (is_empty())
+		error(" 덱 공백 에러");
+	return data[rear];
+}
+
+void printf_deque(const char msg[]) { print_queue(msg); }
+
+void main()
+{
+	int i;
+	init_deque();
+	for (i = 1; i < 10; i++) {
+		if (i % 2) add_front(i);
+		else add_rear(i);
+	}
+	print_queue("원형 덱 홀수-짝수 ");
+	printf("\tdelete_front() --> %d\n", delete_front());
+	printf("\tdelete_rear () --> %d\n", delete_rear());
+	printf("\tdelete_front() --> %d\n", delete_front());
+	print_queue("원형 덱 삭제-홀짝홀");
+}
